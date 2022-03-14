be1650401b6a3356bc718306ccfd3f77915b07eb
Piotr <p.sztuczka@wp.pl>
2022-03-14 22:03:38 +0100

    M2L4 - Dodanie pierwszego beana Springowego

---
+++ src/main/java/pl/sztuczkap/bookstore/Book.java
@@ -0,0 +1,27 @@
+package pl.sztuczkap.bookstore;
+
+import java.util.StringJoiner;
+
+public class Book {
+    Long id;
+    String title;
+    String author;
+    Integer year;
+
+    public Book(Long id, String title, String author, Integer year) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Book.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("title='" + title + "'")
+                .add("author='" + author + "'")
+                .add("year=" + year)
+                .toString();
+    }
+}
--- src/main/java/pl/sztuczkap/bookstore/BookstoreOnlineApplication.java
+++ src/main/java/pl/sztuczkap/bookstore/BookstoreOnlineApplication.java
@@ -1,13 +1,28 @@
 package pl.sztuczkap.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class BookstoreOnlineApplication {
+public class BookstoreOnlineApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreOnlineApplication.class, args);
 	}
 
+	private final CatalogService catalogService;
+
+	public BookstoreOnlineApplication(CatalogService catalogService) {
+		this.catalogService = catalogService;
+	}
+
+	@Override
+	public void run(String... args) {
+//		CatalogService service = new CatalogService();
+		List<Book> books = catalogService.findByTitle("Pan Tadeusz");
+		books.forEach(System.out::println);
+	}
 }
