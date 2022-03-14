a177d257b81aa4437371bc26bda30e2c4c031bdf
Piotr <p.sztuczka@wp.pl>
2022-03-14 20:55:49 +0100

    Start project

---
+++ src/main/java/pl/sztuczkap/bookstore/BookstoreOnlineApplication.java
@@ -0,0 +1,13 @@
+package pl.sztuczkap.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreOnlineApplication.class, args);
+	}
+
+}
