eae3883ee7ddda0ff153059a15b16df65495280a
Piotr <p.sztuczka@wp.pl>
2022-03-15 21:37:29 +0100

    M2L10 - Lombok

--- src/main/java/pl/sztuczkap/bookstore/ApplicationStartup.java
+++ src/main/java/pl/sztuczkap/bookstore/ApplicationStartup.java
@@ -1,5 +1,6 @@
 package pl.sztuczkap.bookstore;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pl.sztuczkap.bookstore.catalog.application.CatalogController;
@@ -8,13 +9,10 @@ import pl.sztuczkap.bookstore.catalog.domain.Book;
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class ApplicationStartup implements CommandLineRunner {
 
     private final CatalogController catalogController;
-
-    public ApplicationStartup(CatalogController catalogController) {
-        this.catalogController = catalogController;
-    }
 
     @Override
     public void run(String... args) {
--- src/main/java/pl/sztuczkap/bookstore/catalog/application/CatalogController.java
+++ src/main/java/pl/sztuczkap/bookstore/catalog/application/CatalogController.java
@@ -1,5 +1,6 @@
 package pl.sztuczkap.bookstore.catalog.application;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import pl.sztuczkap.bookstore.catalog.domain.Book;
 import pl.sztuczkap.bookstore.catalog.domain.CatalogService;
@@ -7,12 +8,9 @@ import pl.sztuczkap.bookstore.catalog.domain.CatalogService;
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class CatalogController {
     private final CatalogService service;
-
-    public CatalogController(CatalogService service) {
-        this.service = service;
-    }
 
     public List<Book> findByTitle(String title) {
         return service.findByTitle(title);
