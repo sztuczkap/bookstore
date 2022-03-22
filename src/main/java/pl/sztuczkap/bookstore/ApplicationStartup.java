bfa6241b24529716653681b34d38385a933b76bd
Piotr <p.sztuczka@wp.pl>
2022-03-22 20:47:44 +0100

    M2L14 - Praca domowa

--- src/main/java/pl/sztuczkap/bookstore/ApplicationStartup.java
+++ src/main/java/pl/sztuczkap/bookstore/ApplicationStartup.java
@@ -15,20 +15,28 @@ public class ApplicationStartup implements CommandLineRunner {
     private final CatalogController catalogController;
     private final String title;
     private final Long limit;
+    private final String author;
 
     public ApplicationStartup(
             CatalogController catalogController,
             @Value("${bookstore.catalog.query}") String title,
-            @Value("${bookstore.catalog.limit}") Long limit
+            @Value("${bookstore.catalog.limit}") Long limit,
+            @Value("${bookstore.catalog.author}") String author
     ) {
         this.catalogController = catalogController;
         this.title = title;
         this.limit = limit;
+        this.author = author;
     }
 
     @Override
     public void run(String... args) {
         List<Book> books = catalogController.findByTitle(title);
         books.forEach(System.out::println);
+
+        System.out.println("---Praca domowa:");
+        List<Book> authors = catalogController.findByAuthor(author);
+        authors.forEach(System.out::println);
+
     }
 }
