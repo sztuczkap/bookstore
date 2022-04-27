3a34b2ce6d39b8fc528d07131c549710b680b0d9
Piotr <p.sztuczka@wp.pl>
2022-04-27 21:09:58 +0200

    M4L2 - Pierwszy Endpoint

---
+++ src/main/java/pl/sztuczkap/bookstore/catalog/web/CatalogController.java
@@ -0,0 +1,22 @@
+package pl.sztuczkap.bookstore.catalog.web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.sztuczkap.bookstore.catalog.application.port.CatalogUseCase;
+import pl.sztuczkap.bookstore.catalog.domain.Book;
+
+import java.util.List;
+
+@RequestMapping("/catalog")
+@RestController
+@AllArgsConstructor
+public class CatalogController {
+    private final CatalogUseCase catalog;
+
+    @GetMapping
+    public List<Book> getAll() {
+        return catalog.findAll();
+    }
+}
